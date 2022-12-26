a696995c2ef326f388982ad8a8d73e345fbc361f
Ryu <ryu@realtime.tw>
2022-12-26 08:18:12 +0800

    add: jwt token in header

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -50,7 +50,11 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch(`http://localhost:8080/feed/posts?page=${page}`)
+    fetch(`http://localhost:8080/feed/posts?page=${page}`, {
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+      },
+    })
       .then((res) => {
         if (res.status !== 200) {
           throw new Error("Failed to fetch posts.");
@@ -123,6 +127,9 @@ class Feed extends Component {
 
     fetch(url, {
       method: method,
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+      },
       body: formData,
     })
       .then((res) => {
@@ -177,6 +184,9 @@ class Feed extends Component {
     this.setState({ postsLoading: true });
     fetch(`http://localhost:8080/feed/post/${postId}`, {
       method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+      },
     })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
